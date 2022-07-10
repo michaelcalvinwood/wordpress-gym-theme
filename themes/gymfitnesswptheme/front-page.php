58d41f938f584acb970fd5a8e9ce162fa2b01b25
Michael Wood <michaelwood33311@icloud.com>
2022-07-10 09:17:54 -0400

    added display of images of the various areas of the gym to the homepage

--- themes/gymfitnesswptheme/front-page.php
+++ themes/gymfitnesswptheme/front-page.php
@@ -3,6 +3,60 @@
     get_header('front'); 
 ?>
 
-<?php 
-   get_footer(); 
-?>
+    <?php while(have_posts()): the_post(); ?>
+
+        <section class="welcome text-center section">
+            <h2 class="text-primary">
+                <?php the_field('welcome_heading'); ?>
+            </h2>
+            <p>
+                <?php the_field('welcome_text'); ?>
+            </p>
+        </section>
+
+        <section class="section-areas">
+            <ul class="areas-container">
+                <li class="area">
+                    <?php
+                        $area1 = get_field('area_1'); 
+                        $image = wp_get_attachment_image_src($area1['area_image'], 'mediumSize')[0];
+                    ?>
+                    <img src="<?php echo $image; ?>" />
+                    <p><?php echo $area1['area_name'] ?></p>
+                </li>
+
+
+                <li class="area">
+                    <?php
+                        $area2 = get_field('area_2'); 
+                        $image = wp_get_attachment_image_src($area2['area_image'], 'mediumSize')[0];
+                    ?>
+                    <img src="<?php echo $image; ?>" />
+                    <p><?php echo $area2['area_name'] ?></p>
+                </li>
+
+
+                <li class="area">
+                    <?php
+                        $area3 = get_field('area_3'); 
+                        $image = wp_get_attachment_image_src($area3['area_image'], 'mediumSize')[0];
+                    ?>
+                    <img src="<?php echo $image; ?>" />
+                    <p><?php echo $area3['area_name'] ?></p>
+                </li>
+
+
+                <li class="area">
+                    <?php
+                        $area4 = get_field('area_4'); 
+                        $image = wp_get_attachment_image_src($area4['area_image'], 'mediumSize')[0];
+                    ?>
+                    <img src="<?php echo $image; ?>" />
+                    <p><?php echo $area4['area_name'] ?></p>
+                </li>
+            </ul>
+        </section>
+
+    <?php endwhile; ?>
+
+<?php get_footer(); ?>
--- themes/gymfitnesswptheme/template-parts/page-loop.php
+++ themes/gymfitnesswptheme/template-parts/page-loop.php
@@ -12,11 +12,11 @@
             if(get_post_type() === 'gymfitness_classes'):
                 $start_time = get_field('start_time');
                 $end_time = get_field('end_time');
-        ?>
-        <p class="content-class">
-            <?php echo the_field('class_days') . " -  " .  $start_time . " to " . $end_time ?>
-        </p>
-        <?php endif;?>
+                ?>
+                <p class="content-class">
+                    <?php echo the_field('class_days') . " -  " .  $start_time . " to " . $end_time ?>
+                </p>
+            <?php endif;?>
         <?php the_content(); ?>
             
 <?php endwhile; ?>
