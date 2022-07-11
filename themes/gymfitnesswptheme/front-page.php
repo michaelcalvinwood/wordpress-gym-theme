1069213e6bf2c7a1270eb95382401c26b395bbe9
Michael Wood <michaelwood33311@icloud.com>
2022-07-11 09:05:30 -0400

    refactored homepage code to tighten it up

--- themes/gymfitnesswptheme/front-page.php
+++ themes/gymfitnesswptheme/front-page.php
@@ -16,44 +16,18 @@
 
         <section class="section-areas">
             <ul class="areas-container">
-                <li class="area">
-                    <?php
-                        $area1 = get_field('area_1'); 
-                        $image = wp_get_attachment_image_src($area1['area_image'], 'mediumSize')[0];
-                    ?>
-                    <img src="<?php echo $image; ?>" />
-                    <p><?php echo $area1['area_name'] ?></p>
-                </li>
-
+                <?php for ($i = 1; $i <= 4; ++$i): ?>
 
                 <li class="area">
                     <?php
-                        $area2 = get_field('area_2'); 
-                        $image = wp_get_attachment_image_src($area2['area_image'], 'mediumSize')[0];
+                        $area = get_field("area_$i"); 
+                        $image = wp_get_attachment_image_src($area['area_image'], 'mediumSize')[0];
                     ?>
                     <img src="<?php echo $image; ?>" />
-                    <p><?php echo $area2['area_name'] ?></p>
+                    <p><?php echo $area['area_name'] ?></p>
                 </li>
 
-
-                <li class="area">
-                    <?php
-                        $area3 = get_field('area_3'); 
-                        $image = wp_get_attachment_image_src($area3['area_image'], 'mediumSize')[0];
-                    ?>
-                    <img src="<?php echo $image; ?>" />
-                    <p><?php echo $area3['area_name'] ?></p>
-                </li>
-
-
-                <li class="area">
-                    <?php
-                        $area4 = get_field('area_4'); 
-                        $image = wp_get_attachment_image_src($area4['area_image'], 'mediumSize')[0];
-                    ?>
-                    <img src="<?php echo $image; ?>" />
-                    <p><?php echo $area4['area_name'] ?></p>
-                </li>
+                <?php endfor; ?>
             </ul>
         </section>
 
